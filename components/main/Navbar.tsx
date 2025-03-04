410251cc6aa2ad8d05be819d384d32c2f3381488
mmk <mmkkarbalaei@gmail.com>
2025-03-04 14:39:39 +0330

    update navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             className='cursor-pointer hover:animate-slowpin'
           />
           <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
-            WebChain Dev
+            MMk Dev
           </span>
         </a>
 
