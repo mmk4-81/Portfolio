f005423eb8613f2bdb938a1d97422ebd69f588fe
mmk4-81 <mmkkarbalaei@gmail.com>
2025-03-12 12:24:03 +0330

    fixed bug

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,6 +3,7 @@ import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
+import * as THREE from "three";
 
 const StarBackground = (props: React.ComponentProps<typeof Points>) => {
   const ref = useRef<THREE.Points>(null!);
